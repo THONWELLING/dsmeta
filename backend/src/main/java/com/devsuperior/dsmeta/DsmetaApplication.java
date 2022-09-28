6aff4a29cc5eaa149fdeead37559b1b4c85beb7d
THONWELLING <thonwellingdani13@gmail.com>
2022-09-28 14:12:26 -0300

    security config

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DsmetaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DsmetaApplication.class, args);
 	}
 
